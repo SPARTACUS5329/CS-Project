e24f0d13168ed5d28449a912a1a3381a64d4c0df
iamgroot9 <58884934+iamgroot9@users.noreply.github.com>
2021-03-08 12:43:59 +0530

    Update artintegration.py

--- artintegration.py
+++ artintegration.py
@@ -150,11 +150,11 @@ def display_graph():
         bg1.append(i[6])
         bg1.append(i[7])
 
-    r1 = [1,2,3,4,5]
+    r1 = [0,1,2,3,4]
     r2 = [i + barWidth for i in r1]
 
-    plt.bar(r1,bg2,width=0.1,label="Class Average")
-    plt.bar(r2,bg1,width=0.1,label="Student")
+    plt.bar(r1,bg2,width=0.2,label="Class Average")
+    plt.bar(r2,bg1,width=0.2,label="Student")
     plt.xlabel('group', fontweight='bold')
     plt.xticks([r + barWidth for r in range(len(bg1))], ['English', 'Physics', 'Chemistry', 'Maths', 'CS'])
 
